68214a18d8e65af0fab6fa2fb7b193b4f49941af
arrjayy <rjaggarwal1993@gmail.com>
2023-08-31 19:45:43 +0100

    Initial MAUI Blazor setup

---
+++ LetsPickAPub/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LetsPickAPub;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ LetsPickAPub/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LetsPickAPub;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
